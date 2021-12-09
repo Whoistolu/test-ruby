b9cd52ca9de39728f97a0d83e307013857d48162
Whoistolu <digittolu25@gmail.com>
2021-12-09 11:07:11 +0100

    Print strings with puts

--- Test.rb
+++ Test.rb
@@ -1,3 +1,9 @@
+=begin
 print 'This is the first string.'
 print 'This is the second string.'
 print 'This is the third string.'
+=end
+
+puts 'This is the first string.'
+puts 'This is the second string.'
+puts 'This is the third string.'
